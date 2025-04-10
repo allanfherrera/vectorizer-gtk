19be1c4cffec890da6ffebd70022d937ee900eff
allanfherrera <92060219+allanfherrera@users.noreply.github.com>
2025-04-09 20:46:36 -0600

    Added main source and precompiled binary file
    Added main source and precompiled binary file

---
+++ line2svg.c
@@ -0,0 +1,100 @@
+#include <gtk/gtk.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct {
+    GtkWidget *window;
+    GtkWidget *button;
+    GtkWidget *label;
+} AppWidgets;
+
+void on_button_clicked(GtkWidget *button, gpointer data) {
+    AppWidgets *widgets = (AppWidgets *)data;
+    
+    GtkWidget *dialog = gtk_file_chooser_dialog_new("Select Line Drawing",
+                                                    GTK_WINDOW(widgets->window),
+                                                    GTK_FILE_CHOOSER_ACTION_OPEN,
+                                                    "_Cancel", GTK_RESPONSE_CANCEL,
+                                                    "_Open", GTK_RESPONSE_ACCEPT,
+                                                    NULL);
+    
+    GtkFileFilter *filter = gtk_file_filter_new();
+    gtk_file_filter_set_name(filter, "Image files");
+    gtk_file_filter_add_pattern(filter, "*.png");
+    gtk_file_filter_add_pattern(filter, "*.jpg");
+    gtk_file_filter_add_pattern(filter, "*.jpeg");
+    gtk_file_filter_add_pattern(filter, "*.bmp");
+    gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(dialog), filter);
+
+    if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
+        char *filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
+        
+        // Create output SVG filename
+        char *svg_filename = malloc(strlen(filename) + 5);
+        strcpy(svg_filename, filename);
+        char *ext = strrchr(svg_filename, '.');
+        if (ext) *ext = '\0';
+        strcat(svg_filename, ".svg");
+
+        // Temporary BMP filename
+        char *bmp_filename = malloc(strlen(filename) + 5);
+        strcpy(bmp_filename, filename);
+        ext = strrchr(bmp_filename, '.');
+        if (ext) *ext = '\0';
+        strcat(bmp_filename, "_temp.bmp");
+
+        // Construct conversion commands optimized for line drawings
+        char command[2048];
+        snprintf(command, sizeof(command), 
+                "convert \"%s\" -threshold 50%% -monochrome \"%s\" && "
+                "potrace -s -k 0.5 -u 10 \"%s\" -o \"%s\" && "
+                "rm \"%s\"",
+                filename, bmp_filename, bmp_filename, svg_filename, bmp_filename);
+
+        // Execute commands
+        int result = system(command);
+        
+        // Update label with result
+        if (result == 0) {
+            gtk_label_set_text(GTK_LABEL(widgets->label), "Line drawing conversion successful!");
+        } else {
+            gtk_label_set_text(GTK_LABEL(widgets->label), "Conversion failed!");
+        }
+
+        g_free(filename);
+        free(svg_filename);
+        free(bmp_filename);
+    }
+
+    gtk_widget_destroy(dialog);
+}
+
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+
+    AppWidgets *widgets = g_new(AppWidgets, 1);
+
+    widgets->window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(widgets->window), "Line Drawing to SVG Converter");
+    gtk_window_set_default_size(GTK_WINDOW(widgets->window), 300, 200);
+    g_signal_connect(widgets->window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    gtk_container_add(GTK_CONTAINER(widgets->window), vbox);
+    gtk_container_set_border_width(GTK_CONTAINER(vbox), 10);
+
+    widgets->label = gtk_label_new("Click to select a line drawing image");
+    gtk_box_pack_start(GTK_BOX(vbox), widgets->label, TRUE, TRUE, 0);
+
+    widgets->button = gtk_button_new_with_label("Select Line Drawing");
+    gtk_box_pack_start(GTK_BOX(vbox), widgets->button, FALSE, FALSE, 0);
+    g_signal_connect(widgets->button, "clicked", G_CALLBACK(on_button_clicked), widgets);
+
+    gtk_widget_show_all(widgets->window);
+
+    gtk_main();
+
+    g_free(widgets);
+
+    return 0;
+}
